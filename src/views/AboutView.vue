ef86db6adfa4e44ac9d2e5eac67a741994fbff46
Emerson602 <emerson.nic@hotmail.com>
2026-01-10 02:04:51 -0300

    update

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -10,9 +10,9 @@
       
       <div class="col-12 col-md-6 mt-5 m-md-0 d-flex justify-content-center align-items-center">
         <p class="col-10 fs-5">
-          Sou Arquiteta e Urbanista formada pela Universidade Federal de Alagoas (UFAL) e atuo no desenvolvimento de projetos que unem estética, funcionalidade e sensibilidade às necessidades de cada cliente.
+          Sou Arquiteta e Urbanista formada pela Universidade Federal de Alagoas (UFAL) e atuo no desenvolvimento de projetos que unem estética, funcionalidade e sensibilidade às necessidades de cada cliente. <br><br>
 
-          Acredito que a arquitetura tem o poder de transformar rotinas e experiências, indo além da forma e da técnica. Meu processo criativo é baseado na escuta atenta, no cuidado com os detalhes e na busca por soluções que respeitem o contexto, o estilo de vida e a identidade de quem irá ocupar o espaço.
+          Acredito que a arquitetura tem o poder de transformar rotinas e experiências, indo além da forma e da técnica. Meu processo criativo é baseado na escuta atenta, no cuidado com os detalhes e na busca por soluções que respeitem o contexto, o estilo de vida e a identidade de quem irá ocupar o espaço. <br><br>
 
           À frente do Estúdio Ilia Arquitetura, conduzo cada projeto de forma próxima e personalizada, garantindo coerência entre conceito, funcionalidade e execução.        </p>
       </div>
